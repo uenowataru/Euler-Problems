979f00a72310b51c463dca475d4395810e4b1813
uenowataru <ueno.wataru@live.com>
2013-09-27 22:53:50 -0400

    solutions for euler problems. may not be optimal.

--- problem1.cpp
+++ problem1.cpp
@@ -13,28 +13,25 @@ using namespace std;
 //returns sum of all the multiples of a or b below n
 class SumMult{
 	public:
-		SumMult(){};
-		~SumMult(){};
 		int sumOfMult(int, int, int);
-		double log(int, int);
 };
 
 int SumMult::sumOfMult(int a, int b, int n){
 	//generates number that can be converted to binary which represents possible combinations
-	int num2a = int(ceil(log(a,n)));
-	int num2b = int(ceil(log(b,n)));
-
-	return num2a + num2b;
-}
-
-double SumMult::log(int base, int x){
-	//return std::log(x)/std::log(base);
-	return 0.1;
+	int sum = 0;
+	for(int num = min(a,b); num < n ; num++){
+		if(num % a == 0 || num % b == 0){
+			sum += num;
+		}
+	}
+	return sum;
 }
 
 int main(){
 	SumMult sm;
 	int sum = sm.sumOfMult(3,5,1000);
+	//sum of 3,5,6,9,10, 12,15,18 = 78
+
 	cout << sum << endl;
 	return 0;
 }
