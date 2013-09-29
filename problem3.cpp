317bd08f3fdb190815decb90d9ca6f63e794f20c
uenowataru <ueno.wataru@live.com>
2013-09-29 15:12:54 -0400

    clean up

--- problem3.cpp
+++ problem3.cpp
@@ -8,19 +8,12 @@ What is the largest prime factor of the number 600851475143 ?
 using namespace std;
 
 class Number{
-  long number;
 public:
-  Number(long);
   long getPrimeFactor(long);
 };
 
-Number::Number(long number){
-  this->number = number;
-}
-
 long Number::getPrimeFactor(long number){
   for(long num = 2; num <= number; num++){
-    //cout<< *prime << " " << *prime%primes[index]<< " " << primes[index] << endl;
     if(number%num == 0){
       if(number/num == 1){
         return num;
@@ -32,7 +25,7 @@ long Number::getPrimeFactor(long number){
 }
 
 int main(){
-  Number number(9);
+  Number number;
   
   cout << "Running.." << endl;
   cout << "Largest prime factor is: " << number.getPrimeFactor(600851475143) <<  endl << endl;
